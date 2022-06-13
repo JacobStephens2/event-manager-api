a7d2c67d418476ddd075ad0e7f33c04e9cb375f5
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-06-13 07:51:50 -0400

    change port listen to 443

--- src/server.js
+++ src/server.js
@@ -15,4 +15,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
-app.listen(8000, () => console.log('Listening on port 8000'));
+app.listen(443, () => console.log('Listening on port 443'));
