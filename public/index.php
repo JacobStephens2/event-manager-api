fe6762db49ffa6c4156fc368954d0d13652c7ca9
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-06-15 19:53:08 -0400

    add headers to response to get '/'

--- public/index.php
+++ public/index.php
@@ -4,12 +4,22 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../initialize.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
+$app->get('/', 
+    function( Request $request, Response $response, $args ) {
+        $response = $response->withHeader('Content-type', 'application/json');
+        $response = $response->withHeader('Access-Control-Allow-Origin', $_ENV['ORIGIN']);
+
+        $message = array('message'=>'Hello world!');
+        $json_message = json_encode($message);
+        $response->getBody()->write($json_message);
+
+        return $response;
+    }
+);
+
 
 $app->run();
