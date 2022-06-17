d02d82d0b2cb2888fd1e57400dd35ec8db71b53a
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-06-16 23:41:26 -0400

    add POST /mimic-json endpoint

--- public/index.php
+++ public/index.php
@@ -8,38 +8,51 @@ require __DIR__ . '/../initialize.php';
 
 $app = AppFactory::create();
 
+// Parse json, form data and xml
+$app->addBodyParsingMiddleware();
+
 // Define app routes
-$app->get('/', function( Request $request, Response $response, $args ) {
+$app->get('/', 
+    function( Request $request, Response $response, $args ) {
         $response = $response->withHeader('Content-type', 'application/json');
         $response = $response->withHeader('Access-Control-Allow-Origin', $_ENV['ORIGIN']);
-
-        $message = array('message'=>'Hello world!');
-        $json_message = json_encode($message);
-        $response->getBody()->write($json_message);
-
+        $message = array(
+            'message'=>'Hello from the Event Manager API',
+            'API Origin'=>$_ENV['API_ORIGIN'],
+            'GET /'=>$_ENV['API_ORIGIN'] . '/',
+            'GET /hello/{name}'=>$_ENV['API_ORIGIN'] . '/hello/Jacob',
+            'POST /mimic-json'=>$_ENV['API_ORIGIN'] . '/mimic-json'
+        );
+        $payload = json_encode($message);
+        $response->getBody()->write($payload);
         return $response;
     }
 );
 
-$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
-});
-
-$app->get('/users', function( Request $request, Response $response, $args ) {
+$app->get('/hello/{name}', 
+    function (Request $request, Response $response, $args) {
         $response = $response->withHeader('Content-type', 'application/json');
         $response = $response->withHeader('Access-Control-Allow-Origin', $_ENV['ORIGIN']);
-
-        $Users = new User;
-        $all_users = $Users->find_all();
-
-        $json_message = json_encode($all_users);
-        $response->getBody()->write($json_message);
-
+        $name = $args['name'];
+        $message = array('message'=>"Hello, $name");
+        $payload = json_encode($message);
+        $response->getBody()->write($payload);
         return $response;
     }
 );
 
+$app->post('/mimic-json', 
+    function( Request $request, Response $response, $args ) {
+        $response = $response->withHeader('Content-type', 'application/json');
+        $response = $response->withHeader('Access-Control-Allow-Origin', $_ENV['ORIGIN']);
+        $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
+        $requestBody = $request->getParsedBody();  
+        $payload = json_encode($requestBody);
+        $response->getBody()->write($payload);
+        return $response;
+    }
+);
+
+$app->addErrorMiddleware($_ENV['ERROR_DISPLAY'], true, true);
 
 $app->run();
