e02001269da022bbc0d08f17b7f3d24461d40e81
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-06-26 20:10:25 -0400

    add method to create record on link table for relating events and clients

--- private/classes/ClientEvent.class.php
+++ private/classes/ClientEvent.class.php
@@ -9,6 +9,16 @@ class ClientEvent extends DatabaseObject {
   public $event_id;
   public $user_id;
 
+  public function create_client_event_by_user_id($client_id, $event_id, $user_id) {
+    $sql = "INSERT INTO clients_events ( ";
+    $sql .= " client_id, event_id, user_id ";
+    $sql .= " ) VALUES ( ";
+    $sql .= $client_id . ", " . $event_id . ", " . $user_id;
+    $sql .= " )";
+    // return $sql;
+    return self::$database->query($sql);
+  }
+
 }
 
 ?>
