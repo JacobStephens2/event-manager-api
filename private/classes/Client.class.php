912fd08a4f6203dbfceb0ba0e8548e72dec0407f
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-06-29 19:22:03 -0400

    store client email

--- private/classes/Client.class.php
+++ private/classes/Client.class.php
@@ -3,11 +3,12 @@
 class Client extends DatabaseObject {
   
   static protected $table_name = 'clients';
-  static protected $db_columns = ['id', 'name', 'user_id'];
+  static protected $db_columns = ['id', 'name', 'user_id', 'email'];
 
   public $id;
   public $name;
   public $user_id;
+  public $email;
 
 }
 
